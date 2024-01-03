2014189ac2ca7d51cf3f20a1f9f2a33fb03dbbf9
Bryan Useche <bryan.useche.10@gmail.com>
2024-01-02 22:18:33 -0400

    cambios en el modulo de comprobantes

--- app/Filament/Resources/ComprobanteResource.php
+++ app/Filament/Resources/ComprobanteResource.php
@@ -9,6 +9,7 @@ use App\Models\Puc;
 use App\Models\TipoDocumentoContable;
 use Filament\Forms;
 use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
@@ -17,13 +18,20 @@ use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\Wizard;
 use Filament\Forms\Form;
 use Filament\Forms\Get;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Support\RawJs;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\TernaryFilter;
 use Icetalker\FilamentTableRepeater\Forms\Components\TableRepeater;
+use NunoMaduro\Collision\Adapters\Phpunit\State;
 
 class ComprobanteResource extends Resource
 {
@@ -32,22 +40,20 @@ class ComprobanteResource extends Resource
     protected static ?string    $navigationIcon = 'heroicon-o-swatch';
     protected static ?string    $navigationLabel = 'Creacion Comprobantes';
     protected static ?string    $navigationGroup = 'Contabilidad';
-    protected static ?string    $navigationParentItem = 'Procesos';   
+    protected static ?string    $navigationParentItem = 'Procesos';
     //protected static ?string    $modelLabel = 'PUC - Cuenta';
 
     public static function form(Form $form): Form
     {
         $query = TipoDocumentoContable::all()->toArray();
         $tipoDocumento = array();
-        foreach($query as $row)
-        {
+        foreach ($query as $row) {
             $tipoDocumento[$row['id']] = "{$row['sigla']} - {$row['tipo_documento']}";
         }
         unset($query);
         $query = Puc::all()->toArray();
         $puc = array();
-        foreach($query as $row)
-        {
+        foreach ($query as $row) {
             $puc[$row['id']] = "{$row['puc']} - {$row['descripcion']}";
         }
         return $form
@@ -58,7 +64,7 @@ class ComprobanteResource extends Resource
                     ->native(false)
                     ->options($tipoDocumento)
                     ->required(),
-                
+
                 TextInput::make('n_documento')
                     ->label('Nº de Documento')
                     ->required(),
@@ -106,28 +112,70 @@ class ComprobanteResource extends Resource
                     ->collapsible()
                     ->defaultItems(1)
                     ->columnSpanFull()
-                    ->visible(fn (Get $get): bool =>  ! $get('is_plantilla')),
+                    ->visible(fn (Get $get): bool =>  !$get('is_plantilla')),
             ]);
     }
 
-    /*public static function table(Table $table): Table
+    public static function table(Table $table): Table
     {
         return $table
             ->columns([
                 //
+                TextColumn::make('id')
+                    ->label('Nº'),
+
+                TextColumn::make('tipoDocumentoContable.id')
+                    ->label('Tipo de Documento Contable'),
+
+                TextColumn::make('n_documento')
+                    ->label('Nº de documento'),
+
+                TextColumn::make('tercero_comprobante')
+                ->label('Tercero Comprobante'),
+
+                TextColumn::make('claseComprobante.id')
+                ->label('Clase comprobante origen'),
+
             ])
             ->filters([
                 //
+                Filter::make('is_plantilla')
+                    ->label('Mostrar solo las plantillas')
+                    ->toggle()
+                    ->default(false),
+
+                Filter::make('created_at')->form([
+                    DatePicker::make('created_from')
+                    ->label('Creado desde')
+                    ->native(false),
+                    DatePicker::make('created_until')
+                    ->label('Creado hasta')
+                    ->native(false)
+                ])
+                ->query(function (Builder $query, array $data): Builder {
+                    return $query
+                    ->when(
+                        $data['created_from'],
+                        fn (Builder $query, $date): Builder => $query->whereDate('created_at', ">=", $date),
+                    )
+                    ->when(
+                        $data['created_until'],
+                        fn (Builder $query, $date): Builder => $query->whereDate('created_at', "<=", $date),
+                    );
+                })
             ])
             ->actions([
+                Tables\Actions\ViewAction::make()
+                ->label('Ver comprobante'),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
+                /*Tables\Actions\BulkActionGroup::make([
                     Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
-    }*/
+                ])*/
+            ])
+            ->emptyStateHeading('Sin comprobantes');
+    }
 
     public static function getRelations(): array
     {
@@ -143,5 +191,53 @@ class ComprobanteResource extends Resource
             'create' => Pages\CreateComprobante::route('/create'),
             'edit' => Pages\EditComprobante::route('/{record}/edit'),
         ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->where('is_plantilla', false);
+    }
+
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist->schema([
+            TextEntry::make('id')
+            ->label('Nº'),
+
+            TextEntry::make('tipo_documento_contables_id')
+            ->label('Tipo de Documento Contable')
+            ->formatStateUsing(fn (State $state): string => dd($state)),
+
+            Section::make('detalle')
+            ->label('Detalle comprobante')
+            ->schema([
+                TableRepeater::make('detalle')
+                    ->label('Detalle comprobante')
+                    ->schema([
+                        TextInput::make('pucs_id')
+                            ->label('Cuenta PUC'),
+
+                        TextInput::make('tercero_registro')
+                            ->label('Tercero Registro'),
+
+                        TextInput::make('descripcion_linea')
+                            ->label('Descripcion Linea'),
+
+                        TextInput::make('debito')
+                            ->placeholder('Debito'),
+
+                        TextInput::make('credito')
+                            ->placeholder('Credito'),
+                    ])
+                    ->reorderable()
+                    ->cloneable()
+                    ->collapsible()
+                    ->defaultItems(1)
+                    ->columnSpanFull()
+                    ->visible(fn (Get $get): bool =>  !$get('is_plantilla'))
+            ])
+
+
+        ]);
     }
 }
