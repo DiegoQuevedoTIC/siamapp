83a3de5aa104a2d4ab1803093aa756e55ee43185
Bryan Useche <bryan.useche.10@gmail.com>
2024-02-18 12:49:18 -0400

    modificacion en la puc para ser cuentas recursivas

--- app/Http/Controllers/CierreMensualController.php
+++ app/Http/Controllers/CierreMensualController.php
@@ -8,6 +8,7 @@ use App\Models\Comprobante;
 use App\Models\ComprobanteLinea;
 use App\Models\Puc;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CierreMensualController extends Controller
 {
@@ -15,22 +16,20 @@ class CierreMensualController extends Controller
 
     public function index()
     {
-        /*Obtenemos las pucs*/
-        $puc = Puc::all()->toArray();
-        /*buscamos los saldos anteriores en el cierre mensual*/
-        foreach($puc as $key => $value){
-            $query = CierreMensualDetalle::where('puc_id', '', $value['id']);
-            if(empty($query))
-            {
-               $saldoAnterior = 0.00;
-               /*Insertamos el registro para inicializar el asiento*/
-               $cierreMensual = [
-                'fecha' => '2024-02-15',
-                'mes_cierre' => 'Febrero',
-                'user_id' => 1
-               ];
+        /*Obtenemos todos los comprobantes del mes*/
+        $comprobantes = Comprobante::whereBetween('fecha_comprobante', '2024-02-01', '2024-02-29');
+        /*Puede que no devuelva ningun comprobante, para este caso, inicializamos todas las cuentas con 0.00*/
+        if(empty($comprobantes))
+        {
+            $cierreMensual = new CierreMensual();
+            $cierreMensual->fecha_cierre = '2024-03-01';
+            $cierreMensual->mes_cierre = 'Febrero';
+            $cierreMensual->user_id = Auth::id();
+            $cierreMensual->created_at = date('Y-m-d h:i:s');
+            $cierreMensual->save();
+            /*Obtenemos el id para insertar las lineas restantes*/ 
+            
 
-            }
         }
         
     }
